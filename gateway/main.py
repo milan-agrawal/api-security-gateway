bd83f4f5b503c5b8d79ab1b135ddcc43a9a6e1fa
HASAN-CE <hasanrangooni308@gmail.com>
2025-12-21 13:02:40 +0530

    feat: implement API Security Gateway with authentication and reverse proxy

--- gateway/main.py
+++ gateway/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request, Response
+from fastapi import FastAPI, Request, Response, HTTPException
 import requests
 import asyncio
 
@@ -6,8 +6,21 @@ app = FastAPI(title="API Security Gateway")
 
 BACKEND_URL = "http://127.0.0.1:9000"
 
+VALID_API_KEYS = {
+    "secret123",
+    "client-demo-key",
+    "Testing-API-Key"
+}
+
 @app.api_route("/{path:path}", methods=["GET", "POST"])
 async def proxy(request: Request, path: str):
+
+    api_key=request.headers.get("X-API-KEY")
+    if api_key not in VALID_API_KEYS:
+            raise HTTPException(
+            status_code=401,
+            detail="Unauthorized: Invalid or missing API key"
+        )
     body = await request.body()   # ✅ await the coroutine
 
     backend_response = await asyncio.to_thread(
